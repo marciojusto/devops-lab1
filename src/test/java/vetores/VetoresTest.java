73711b04d9a829c4610f94d7927e5b3c854e11b0
Marcio Oliveira <marcio.oliveira@atradius.com>
2019-12-19 13:22:57 +0000

    Início da classe de teste.

---
+++ src/test/java/vetores/VetoresTest.java
@@ -0,0 +1,17 @@
+package vetores;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class VetoresTest {
+
+    String[] vetor = {"Texto 1", "Texto 2"};
+    Vetores v = new Vetores();
+
+    @Test
+    public void deveRetornarElementoDeAcordoComIndice() {
+
+    }
+}
